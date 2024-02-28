33978704a38ff613ed6ed0021f67482f29621790
Faizanahmedsy <saiyed.faizanahmed1@gmail.com>
2024-02-28 22:53:02 +0530

    feat: finished the registration form

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -21,6 +21,11 @@ export default function RegisterShopPage() {
     resolver: zodResolver(registrationSchema),
     defaultValues: {
       firstName: "",
+      lastName: "",
+      username: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
     },
   });
 
@@ -30,80 +35,119 @@ export default function RegisterShopPage() {
 
   return (
     <>
-      <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-          <div className="flex gap-5">
-            <FormField
-              control={form.control}
-              name="firstName"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>First Name</FormLabel>
-                  <FormControl>
-                    <Input placeholder="shadcn" {...field} />
-                  </FormControl>
+      <div className="flex flex-col w-[700px]">
+        <h1 className="text-3xl font-semibold my-5">Register</h1>
+        <hr />
+        <div className="my-5" />
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="space-y-8 w-[700px] mx-auto my-5"
+          >
+            <div className="flex gap-5">
+              <FormField
+                control={form.control}
+                name="firstName"
+                render={({ field }) => (
+                  <FormItem className="w-full">
+                    <FormLabel>First Name</FormLabel>
+                    <FormControl>
+                      <Input placeholder=" " {...field} />
+                    </FormControl>
 
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="username"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Last Name</FormLabel>
-                  <FormControl>
-                    <Input placeholder="shadcn" {...field} />
-                  </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="lastName"
+                render={({ field }) => (
+                  <FormItem className="w-full">
+                    <FormLabel>Last Name</FormLabel>
+                    <FormControl>
+                      <Input placeholder=" " {...field} />
+                    </FormControl>
 
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-          </div>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
 
-          <div className="flex gap-5">
-            <FormField
-              control={form.control}
-              name="username"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Username</FormLabel>
-                  <FormControl>
-                    <Input placeholder="shadcn" {...field} />
-                  </FormControl>
+            <div className="flex gap-5">
+              <FormField
+                control={form.control}
+                name="username"
+                render={({ field }) => (
+                  <FormItem className="w-full">
+                    <FormLabel>Username</FormLabel>
+                    <FormControl>
+                      <Input placeholder=" " {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem className="w-full">
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input placeholder=" " {...field} />
+                    </FormControl>
 
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="username"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Username</FormLabel>
-                  <FormControl>
-                    <Input placeholder="shadcn" {...field} />
-                  </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
 
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-          </div>
+            <div className="flex gap-5">
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem className="w-full">
+                    <FormLabel>Password</FormLabel>
+                    <FormControl>
+                      <Input placeholder=" " {...field} />
+                    </FormControl>
 
-          <div className="flex justify-between items-center gap-6">
-            {/* <Button variant={"secondary"} className="w-full">
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name="confirmPassword"
+                render={({ field }) => (
+                  <FormItem className="w-full">
+                    <FormLabel>Confirm Password</FormLabel>
+                    <FormControl>
+                      <Input placeholder=" " {...field} />
+                    </FormControl>
+
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+
+            <div className="flex justify-between items-center gap-6">
+              {/* <Button variant={"secondary"} className="w-full">
               Submit
             </Button> */}
-            <Button type="submit" size={"sm"} className="w-full">
-              Submit
-            </Button>
-          </div>
-        </form>
-      </Form>
+              <Button type="submit" size={"sm"} className="w-full">
+                Submit
+              </Button>
+            </div>
+          </form>
+        </Form>
+      </div>
     </>
   );
 }
--- src/schema/auth-schema.ts
+++ src/schema/auth-schema.ts
@@ -1,10 +1,50 @@
 import { z } from "zod";
 
-export const registrationSchema = z.object({
-  firstName: z.string().min(2).max(50),
-  lastName: z.string().min(2).max(50),
-  username: z.string().min(3).max(50),
-  email: z.string().email(),
-  password: z.string().min(8).max(50),
-  confirmPassword: z.string().min(8).max(50),
-});
+export const registrationSchema = z
+  .object({
+    firstName: z
+      .string()
+      .min(2, {
+        message: "First name must be at least 2 characters",
+      })
+      .max(50)
+      .refine((value) => /^[a-zA-Z]+$/.test(value), {
+        message: "First name must not contain special characters or numbers",
+      }),
+    lastName: z
+      .string()
+      .min(2, {
+        message: "Last name must be at least 2 characters",
+      })
+      .max(50)
+      .refine((value) => /^[a-zA-Z]+$/.test(value), {
+        message: "Last name must not contain special characters or numbers",
+      }),
+    username: z
+      .string()
+      .min(3, {
+        message: "Username must be at least 3 characters",
+      })
+      .max(50)
+      .refine((value) => /^[a-z]+$/.test(value), {
+        message: "Username must be in lowercase",
+      }),
+    email: z.string().email(),
+    password: z
+      .string()
+      .min(8, {
+        message: "Password must be at least 8 characters",
+      })
+      .max(50)
+      .refine((value) => /[0-9]/.test(value), {
+        message: "Password must contain at least one number",
+      })
+      .refine((value) => /[!@#$%^&*(),.?":{}|<>]/.test(value), {
+        message: "Password must contain at least one special character",
+      }),
+    confirmPassword: z.string().min(8).max(50),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords do not match",
+    path: ["confirmPassword"],
+  });
