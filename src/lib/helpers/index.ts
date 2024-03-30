e1b36072cb59f5db601df0fd0ef91d57b47d8ee2
Faizanahmedsy <saiyed.faizanahmed1@gmail.com>
2024-03-30 16:36:40 +0530

    added super token formatted

--- src/lib/helpers/index.ts
+++ src/lib/helpers/index.ts
@@ -1,3 +1,5 @@
+import { jwtDecode } from "jwt-decode";
+
 export const extractRoleFromToken = (decodedToken: any) => {
   const roleKey: any = Object.keys(decodedToken).find((key) =>
     key.includes("identity/claims/role")
@@ -10,4 +12,28 @@ export const extractEmailFromToken = (decodedToken: any) => {
     key.includes("identity/claims/emailaddress")
   );
   return decodedToken[roleKey];
+};
+
+export const getDecodedToken = (token: any) => {
+  let accessToken: any = token;
+
+  let decodedAccessToken: any = jwtDecode(accessToken);
+
+  return decodedAccessToken;
+};
+
+export const superTokenFormatter = (token: any) => {
+  let accessToken: any = token;
+
+  let decodedAccessToken: any = jwtDecode(accessToken);
+
+  const formattedToken = {
+    emailAddress: extractEmailFromToken(decodedAccessToken),
+    role: extractRoleFromToken(decodedAccessToken),
+    isComplete: decodedAccessToken?.isComplete,
+    isEmailVerified: decodedAccessToken?.isEmailVerified,
+    isVerified: decodedAccessToken?.isVerified,
+  };
+
+  return formattedToken;
 };
